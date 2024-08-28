0aba83431ed20e475ec778f632131857b0329529
Corinne Mainardi <mainardi.corinne@gmail.com>
2024-08-28 21:33:42 +0200

    esercizio completo senza css

--- assets/w3d3.js
+++ assets/w3d3.js
@@ -1,10 +1,60 @@
 //creo evento che scatena caricamento della pagina
 
-window.onload = () => {
-  console.log("carimento della pagina andato correttamente a buon fine");
+const input = document.getElementById("newTask");
+
+const button = document.getElementById("addTask");
+const main = document.querySelector("main");
+const div = document.getElementById("error");
+
+const createUl = (event) => {
+  event.preventDefault();
+  let ul = document.querySelector("ul");
+  if (!ul) {
+    //se non esiste entro e creo ul
+    ul = document.createElement("ul");
+    main.appendChild(ul);
+  }
+  addTask();
 };
 
-const secondDiv = document.querySelector("main> div form div");
-const UlList = document.createElement("ul");
-UlList.classList.add("todo-list");
-secondDiv.appendChild(UlList);
+const addTask = () => {
+  let task = input.value;
+  let ul = document.querySelector("ul");
+  const li = document.createElement("li");
+  const buttonLi = document.createElement("button");
+  const deleteLi = document.createElement("button");
+
+  li.innerText = task;
+  buttonLi.innerText = "fatto";
+  deleteLi.innerHTML = "&#128465";
+
+  buttonLi.addEventListener("click", () => {
+    li.style.textDecoration = "line-through";
+  });
+  deleteLi.addEventListener("click", () => {
+    li.remove();
+  });
+  ul.appendChild(li);
+  if (task === "") {
+    li.remove();
+
+    let p = document.querySelector(".errorP");
+    if (p) {
+      p.remove();
+    }
+    p = document.createElement("p");
+    p.classList.add("errorP");
+    p.innerText = "input vuoto! Scrivi qualcosa";
+    main.appendChild(p);
+  } else {
+    let p = document.querySelector(".errorP");
+    if (p) {
+      p.remove();
+    }
+  }
+
+  li.appendChild(buttonLi);
+  li.appendChild(deleteLi);
+  input.value = "";
+};
+button.addEventListener("click", createUl);
